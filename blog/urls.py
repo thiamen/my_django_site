40502d0daf2e1524d2a4ab238c9fc6ecbd91d9f2
Mouhamadou Thiam <calzman@yahoo.com>
2020-08-17 21:02:01 -0700

    Addfeature to approve comment post and a count of number of comments.

--- blog/urls.py
+++ blog/urls.py
@@ -37,6 +37,7 @@ urlpatterns = [
     # path would look like 127.0.0.1:8000/comment/2/remove ----> local
     # mydjangosite.com/comment/2/remove ---> online
     path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
 ]
 
 
--- blog/views.py
+++ blog/views.py
@@ -81,6 +81,10 @@ def comment_remove(request, pk):
     comment.delete()
     return redirect('post_detail', pk=comment.post.pk)
 
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
 
 
 
